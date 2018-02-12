3bfaed719a51c1a05a3a607f41550ae91bd60cdc
KURO-ASUS\Kuro <darkip.register@gmail.com>
2018-02-12 16:16:48 +0800

    Placeholder for player

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
+    public float movementSpd = 10;
+
+    private float rotSpd = 10;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,11 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 newPos = transform.position + new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * movementSpd * Time.deltaTime;
+        Vector3 targetDir = newPos - transform.position;
+        float step = rotSpd * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDir);
+        transform.position = newPos;
 	}
 }
