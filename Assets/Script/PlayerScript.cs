750942ecffab5eb8739a07a6487774a046bf2f47
SIDM\160353B <janraphaelo1709@gmail.com>
2018-02-13 17:26:21 +0800

    Enemy AI chases player, Boss script in progress

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -7,19 +7,24 @@ public class PlayerScript : MonoBehaviour {
     public float movementSpd = 10;
 
     private float rotSpd = 10;
-
-	// Use this for initialization
-	void Start () {
+    Vector3 newPos;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 newPos = transform.position + new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * movementSpd * Time.deltaTime;
+        newPos = transform.position + new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * movementSpd * Time.deltaTime;
         Vector3 targetDir = newPos - transform.position;
         float step = rotSpd * Time.deltaTime;
         Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
         transform.rotation = Quaternion.LookRotation(newDir);
         transform.position = newPos;
 	}
+
+    public Vector3 Get_Player_Pos()
+    {
+        return newPos;
+    }
 }
