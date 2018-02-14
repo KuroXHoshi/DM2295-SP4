d8b2c6e1b5d57add1c8e0b36ced76e482c5a12e2
LAPTOP-CDK6IJOA\Ygg <wilfredmaple@gmail.com>
2018-02-14 09:25:40 +0800

    added main menu and settings UI, with loading screen, also audio

---
+++ Assets/Script/LoaderScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoaderScript : MonoBehaviour {
+    //[SerializeField]
+    //Canvas mainMenu;
+    public GameObject LoadScreen;
+    public Slider slider;
+    public Text progresstext;
+
+    public void Loadlevel(int SceneIndex)
+    {
+       // mainMenu.enabled = false;
+        StartCoroutine(LoadAsynchronously(SceneIndex));
+    }
+    IEnumerator LoadAsynchronously(int SceneIndex)
+    {
+        //mainMenu.enabled = false;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneIndex);
+        LoadScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+            progresstext.text = progress * 100f + "%";
+            yield return null;
+        }
+    }
+}
+    
+
---
+++ Assets/Script/SceneTransit.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneTransit : MonoBehaviour {
+
+    [SerializeField]
+    Canvas mainMenu;
+
+    [SerializeField]
+     Canvas optionsCanvas;
+
+    Canvas currentCanvas;
+
+    [SerializeField]
+    Canvas FriendlistCanvas;
+
+    Canvas TimerCanvas;
+
+    public Text timertext;
+    public float startertime;
+    protected void Start()
+    {
+       // startertime = Time.time;
+        optionsCanvas.enabled = false;
+    }
+    public void Game()
+    {
+        //startertime = Time.time;
+        //TimerCanvas.enabled = true;
+        //float timer = 0;
+        //timer = Time.deltaTime * 10; // go second by seconds
+    }
+    void Update()
+    {
+        //float timer = Time.time - startertime;
+
+        //string Minutes = ((int)timer / 60).ToString();
+        //string Seconds = (timer % 60).ToString();
+
+    //    timertext.text = Minutes + ":" + Seconds;
+        //if (timer > 5)
+        //{
+        //   // TimerCanvas.enabled = false;
+        //    SceneManager.LoadScene("InGame"); //load title of scene
+        //    timer = 0;
+        //}
+        //Debug.Log("timer running" + timer);
+}
+
+public void Options()
+    {
+        mainMenu.enabled = false;
+        optionsCanvas.enabled = true;
+
+        currentCanvas = optionsCanvas;
+    }
+
+    public void HighScore()
+    {
+        mainMenu.enabled = false;
+        //highscoreCanvas.enabled = true;
+
+        //currentCanvas = highscoreCanvas;
+    }
+    public void Back() // options back btn, highscore back btn
+    {
+        mainMenu.enabled = true;
+
+        //if option, options canvas disable, 
+        //if highscore, highscore canvas disable
+
+        currentCanvas.enabled = false;
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
