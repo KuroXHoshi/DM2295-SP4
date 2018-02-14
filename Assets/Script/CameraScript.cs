ae218af88c63615d9e3402bbbbf7b7f5b1c176cd
KURO-ASUS\Kuro <darkip.register@gmail.com>
2018-02-14 16:59:26 +0800

    Commiting additional files

---
+++ Assets/Script/CameraScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private float defaultHeight, defaultZ;
+
+    private void Awake()
+    {
+        if (!(player = GameObject.FindGameObjectWithTag("Player").transform))
+            Debug.Log("Camera.cs: Unable to load Player!");
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        transform.position = new Vector3(13, defaultHeight, 6 + defaultZ);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        float offsetX = 0, offsetZ = 0;
+
+        if (player.position.x > 13 && player.position.x < 25)
+        {
+            offsetX = player.position.x - transform.position.x;
+        }
+
+        if (player.position.z > 6 && player.position.z < 29)
+        {
+            offsetZ = player.position.z - transform.position.z + defaultZ;
+        }
+
+        transform.position += new Vector3(offsetX, 0, offsetZ) * 5 * Time.deltaTime;
+    }
+}
---
+++ Assets/Script/PlayerSkills.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkills : MonoBehaviour
+{
+
+    public void Dash(ref Vector3 _translation, ref float _stamina)
+    {
+        //_translation += _transform.forward * 5f;
+        _stamina -= 1;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
