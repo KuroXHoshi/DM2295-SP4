f3bf6cc4f0793cef00b8e6fcb5567fdf325c9e6e
LAPTOP-CDK6IJOA\Ygg <wilfredmaple@gmail.com>
2018-02-20 11:09:15 +0800

    added minimap and player icon

---
+++ Assets/Script/MinimapScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+    public Transform player;
+    // Use this for initialization
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = player.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+
+        
+    }
+}
