e108e2af835d56e0fa7181e30151848543ad75c9
HoopAWolf <hoopawolf394@gmail.com>
2018-02-21 17:37:56 +0800

    StateMelee
    StateMelee

---
+++ Assets/Script/StateMelee.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMelee : State
+{
+    public StateMelee(string _stateID) : base(_stateID)
+    {
+
+    }
+	
+	// Update is called once per frame
+    override
+	public void Update ()
+    {
+		
+	}
+}
