3902d8616d0046c239a5db7d3ce2b73bd21c64f5
KURO-ASUS\Kuro <darkip.register@gmail.com>
2018-02-21 15:03:11 +0800

    Pushing StateMachine manager

---
+++ Assets/Script/StateMachine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    Dictionary<string, State> StateMap;
+    State CurrState = null, NextState = null;
+
+    public void AddState(State newState)
+    {
+        if (newState != null || StateMap.ContainsKey(newState.GetStateID()))
+            return;
+
+        if (CurrState != null)
+            CurrState = NextState = newState;
+
+        StateMap.Add(newState.GetStateID(), newState);
+    }
+
+    public void SetNextState(string nextStateID)
+    {
+        if (CurrState.GetStateID() == nextStateID)
+            return;
+
+        if (!StateMap.TryGetValue(nextStateID, out NextState))
+            Debug.Log(this.GetType() + ".cs : Unable to SetNextState!");
+    }
+
+    public string GetCurrentState()
+    {
+        if (CurrState != null)
+            return CurrState.GetStateID();
+        return "<No States>";
+    }
+
+    // Update is called once per frame
+    public void Update ()
+    {
+		if (CurrState != NextState)
+        {
+            CurrState.Exit();
+            CurrState = NextState;
+            CurrState.Enter();
+        }
+
+        CurrState.Update();
+	}
+}
