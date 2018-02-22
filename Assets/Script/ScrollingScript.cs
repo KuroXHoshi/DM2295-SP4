ae7dd750355890e23f22b4f52bb76f808424a29f
Ygg\Wilfred <Wilfredmaple@gmail.com>
2018-02-22 14:34:17 +0800

    scrolling background for main menu

---
+++ Assets/Script/ScrollingScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingScript : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+	// Update is called once per frame
+	void Update () {
+      Vector2 offSet = new Vector2(0, Time.time * speed)  ;
+        GetComponent<Renderer>().material.mainTextureOffset = offSet;
+	}
+}
